d46939055841081f68336c0d600d6acbfb9089d7
KyleP <kyl3p3tty@gmail.com>
2022-03-20 19:56:23 -0700

    added script for quiz questions to cycle through

--- assets/script.js
+++ assets/script.js
@@ -49,6 +49,51 @@ var resetQuizBtn = document.getElementById("resetQuiz");
 var highscoreList = document.getElementById("highscore-list");
 var highscoreReset = document.getElementById("resetHighscore");
 
+var startQuiz = function() {
+mainMenu.style.display = 'none';
+quizMenu.style.display = 'block';
+setTimer();
+setQuestion();
+}
+
+var setQuestion = function(){
+questionEl.textContent = questionsArray[questionNum].question;  
+for (let i = 0; i < 4; i++) {
+    choiceList[i].textContent = questionsArray[questionNum].choices[i];
+}
+choicesEl.addEventListener("click", isCorrect);
+}
+
+var isCorrect = function(event){
+
+var choice = event.target;
+if (choice.textContent === questionsArray[questionNum].answer){
+    choice.classList.add("list-group-item-success");
+    choice.textContent += " Correct!";
+    score += 5;
+} else {
+    choice.classList.add("list-group-item-danger");
+    choice.textContent += " Incorrect!";
+    score -= 1;
+    if(score <= 0){score = 0;}
+}
+choicesEl.removeEventListener("click", isCorrect);
+setTimeout(nextQuestion, 500, choice);
+}
+
+var nextQuestion = function(choice){
+
+choice.classList.remove("list-group-item-danger");
+choice.classList.remove("list-group-item-success");
+questionNum++;
+if(questionNum < questionsArray.length){
+    setQuestion();  
+} else if (questionNum = questionsArray.length){
+    questionsFinished = true;
+    setupHighscore();
+}
+
+}
 
 
 beginQuiz.addEventListener("click", startQuiz);
